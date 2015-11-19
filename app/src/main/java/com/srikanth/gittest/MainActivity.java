96c7c41baa7d2ccab6dc1a4fb1e0d88479af26ab
srikanthPrakash <srikanthp91@gmail.com>
2015-11-19 19:10:23 +0530

    Commit Two

--- app/src/main/java/com/srikanth/gittest/MainActivity.java
+++ app/src/main/java/com/srikanth/gittest/MainActivity.java
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //New Content Added
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
